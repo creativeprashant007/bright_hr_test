d57a2a668e91772f0e96b23e900c2b089a329067
creativeprashant007 <prashanttimalsina18@gmail.com>
2025-03-05 08:08:32 +0000

    fix:constants added

--- lib/common/values/constants.dart
+++ lib/common/values/constants.dart
@@ -1,3 +1,8 @@
 class AppConstants {
   static const String SERVER_API_URL = "https://jsonplaceholder.typicode.com";
+  static const String POSTS_API_URL = "$SERVER_API_URL/posts";
+
+
+  //hive 
+  static const String SAVE_POST_HIVE_KEY = "saved_posts";
 }
