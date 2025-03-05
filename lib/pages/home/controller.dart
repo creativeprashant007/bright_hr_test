63c021480b0a82d3489a39d236ece1be9b8f845a
creativeprashant007 <prashanttimalsina18@gmail.com>
2025-03-05 08:09:22 +0000

    feat:home controller

---
+++ lib/pages/home/controller.dart
@@ -0,0 +1,48 @@
+import 'package:bright_hr_posts/common/routes/route_names.dart';
+import 'package:bright_hr_posts/common/entities/post.dart';
+import 'package:bright_hr_posts/pages/home/bloc/home_bloc.dart';
+import 'package:bright_hr_posts/pages/home/bloc/home_events.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePageController {
+  final BuildContext context;
+  late TabController tabController;
+
+  HomePageController({required this.context, required TickerProvider vsync}) {
+    tabController = TabController(length: 2, vsync: vsync);
+    _initialize();
+  }
+
+  void _initialize() {
+    // Fetch posts and load offline posts
+    context.read<HomeBloc>().add(FetchPostsEvents());
+    context.read<HomeBloc>().add(LoadOfflinePostsEvents());
+
+    // Listen for tab changes
+    tabController.addListener(() {
+      if (tabController.indexIsChanging) {
+        context.read<HomeBloc>().add(HomeTabChangedEvents(tabController.index));
+      }
+    });
+  }
+
+  void navigateToDetailsPage(Post post, bool isSaved) async {
+    await Navigator.pushNamed(
+      context,
+      RouteNames.POST_DETAILS_PAGE,
+      arguments: {'post_id': post.id, 'is_saved': isSaved},
+    );
+
+    // Reload saved posts after returning
+    context.read<HomeBloc>().add(LoadOfflinePostsEvents());
+  }
+
+  void savePost(Post post) {
+    context.read<HomeBloc>().add(SavePostEvents(post));
+  }
+
+  void removePost(int postId) {
+    context.read<HomeBloc>().add(RemovePostEvents(postId));
+  }
+}
